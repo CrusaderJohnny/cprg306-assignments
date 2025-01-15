cd4ce42bc0e737a6b345bab1aa958b44aaa32812
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-01-15 13:45:21 -0700

    modifications

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,15 +1,7 @@
-import Title from "./title.js";
-
 export default function Page() {
-    let a =10;
-    let b =2;
-    return(
-        <main>
-            <Title />
-            <p>This is the second page</p>
-            <p>
-                The sum of {a} and {b} is {a+b}
-            </p>
-        </main>
+    return (
+      <main>
+        <h1>Shopping List</h1>
+      </main>
     );
-}
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+    return(
+        <main>
+        <p>Mark Leonardis</p>
+        <p>Github profile located <Link href="https://github.com/CrusaderJohnny">Here</Link></p>
+        </main>
+    );
+}
