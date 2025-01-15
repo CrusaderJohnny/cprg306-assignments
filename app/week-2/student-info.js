af31eb6a380a65735d2dc0cde66a313acd43453c
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-01-15 13:54:22 -0700

    link fix

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,8 +1,7 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
     return(
-        <main>
-        <p>Mark Leonardis</p>
-        <p>Github profile located <Link href="https://github.com/CrusaderJohnny">Here</Link></p>
-        </main>
+        <><p>Mark Leonardis</p><p>Github located at <Link href="https://github.com/CrusaderJohnny">Here</Link></p></>
     );
 }
