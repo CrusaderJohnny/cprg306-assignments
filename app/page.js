a9b340e31e9d43660a06ae231c3a8e3c83fa2f59
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-01-15 13:24:48 -0700

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  <div>
-    <h1>
-      Words go here
-    </h1>
-  </div>
+  return(
+    <div>
+      <h1>
+        Words go here
+      </h1>
+    </div>
+  );
 }
