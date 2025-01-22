784c025f35bd07c4e7f2ca947ec4ec3bd8b3dd1f
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-01-22 13:35:03 -0700

    garbage in garbage out

---
+++ app/week-3/item.js
@@ -0,0 +1,2 @@
+export default function Items( { name, qunatity, category } ) {
+}
---
+++ app/week-3/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Items from './item';
+import { ItemList } from './item-list';
+import Item from './item';
+
+export default function Page() {
+    return(
+        <div>
+            <h1 className='font-bold text-4xl p-2 m-2'>Shopping List</h1>
+            <ItemList />
+        </div>        
+    );
+}
