873bf0b5f749eb47c524e391d16eb315822eb7d7
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-02-20 17:34:06 -0700

    week-7 state management.

---
+++ app/week-7/item-list.js
@@ -0,0 +1,102 @@
+"use client";
+import Item from "./item";
+import React, { useState } from "react";
+
+export function ItemList({ items }) {
+  const [sortBy, setSortBy] = useState("name");
+
+  const handleSortchange = (newSortBy) => {
+    setSortBy(newSortBy);
+  };
+
+  const sortingHat = [...items].sort((a, b) => {
+    if (sortBy === "name") {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    } else if (sortBy === "category") {
+      if (a.category < b.category) {
+        return -1;
+      }
+      if (a.category > b.category) {
+        return 1;
+      }
+      return 0;
+    } else {
+      return 0;
+    }
+  });
+
+  const groupingHat = () => {
+    if (sortBy === "grouped") {
+      const grouped = items.reduce((acc, item) => {
+        const category = item.category;
+        if (!acc[category]) {
+          acc[category] = [];
+        }
+        acc[category].push(item);
+        return acc;
+      }, {});
+      const sortedCategories = Object.keys(grouped).sort();
+      for (const category in grouped) {
+        grouped[category].sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      return sortedCategories.map((category) => (
+        <div key={category}>
+          <h2 className="capitalize">{category}</h2>
+          {grouped[category].map((item) => (
+            <Item key={item.id} {...item} />
+          ))}
+        </div>
+      ));
+    } else {
+      return sortingHat.map((item) => <Item key={item.id} {...item} />);
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-items-start p-5">
+        <button
+          onClick={() => handleSortchange("name")}
+          style={{ backgroundColor: sortBy === "name" ? "lightgray" : "white" }}
+          className="text-black p-2"
+        >
+          Sort By Name
+        </button>
+        <div className="p-2"></div>
+        <button
+          onClick={() => handleSortchange("category")}
+          style={{
+            backgroundColor: sortBy === "category" ? "lightgrey" : "white",
+          }}
+          className="text-black p-2"
+        >
+          Sort by Category
+        </button>
+        <div className="p-2"></div>
+        <button
+          onClick={() => handleSortchange("grouped")}
+          style={{
+            backgroundColor: sortBy === "category" ? "lightgrey" : "white",
+          }}
+          className="text-black p-2"
+        >
+          Group by Category
+        </button>
+      </div>
+      <div>{groupingHat()}</div>
+    </div>
+  );
+}
---
+++ app/week-7/page.js
@@ -0,0 +1,25 @@
+"use client";
+import React, { useState } from "react";
+import { ItemList } from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  const itemHandler = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
+
+  return (
+    <div className="flex flex-row">
+      <div>
+        <h1 className="font-bold text-4xl p-2 m-2">Shopping List</h1>
+        <ItemList items={items} />
+      </div>
+      <div>
+        <NewItem onAddItem={itemHandler} />
+      </div>
+    </div>
+  );
+}
